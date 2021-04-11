c0a9dfb2ca9d5fc54cd5087cf04418addcec8a62
Ole Petersen <peteole2707@gmail.com>
2021-04-11 12:21:09 +0200

    Added mpu92dmp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include "DataSchema.h"
+#include "Message.h"
+#include "MessageRegistry.h"
 
-void setup() {
+// Initialize sensor values
+
+FloatSensorValue pitch("pitch");
+FloatSensorValue bank("bank");
+FloatSensorValue speed("speed");
+Int16SensorValue heading("heading");
+FloatSensorValue altitude("altitude");
+Int32SensorValue time("time");
+SensorValue *basicValueList[] = {&pitch, &bank, &speed, &altitude, &heading, &time};
+SensorValueList package(basicValueList, 6);
+Message basics(&package, 17);
+MessageRegistry registry;
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
