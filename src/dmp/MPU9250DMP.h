9c2af613242424c8c0087ab615d3f351944d34ad
Ole Petersen <peteole2707@gmail.com>
2021-04-11 13:29:59 +0200

    Got hc12 communication working

---
+++ src/dmp/MPU9250DMP.h
@@ -0,0 +1,26 @@
+// file for motion procession for artificial horizon and compass
+#define MPU9250_SENSOR
+#ifndef MPU9250DMP_H
+#define MPU9250DMP_H
+#include "dmp.h"
+#include "Wire.h"
+#include "MPU9250.h"
+
+class MPU9250DMP : public DMP
+{
+public:
+    MPU9250 mpu;
+    MPU9250DMP(TwoWire medium = Wire) : mpu(medium, 0x68)
+    {
+    }
+    void readGyro() override;
+    void readAccel() override;
+    // begin sensor reading
+    void begin();
+    void calibrateGyro();
+
+private:
+    long lastRead;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include "DataSchema.h"
 #include "Message.h"
 #include "MessageRegistry.h"
+#include "dmp/MPU9250DMP.h"
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
 
 // Initialize sensor values
-
 FloatSensorValue pitch("pitch");
 FloatSensorValue bank("bank");
 FloatSensorValue speed("speed");
@@ -15,12 +17,51 @@ SensorValue *basicValueList[] = {&pitch, &bank, &speed, &altitude, &heading, &ti
 SensorValueList package(basicValueList, 6);
 Message basics(&package, 17);
 MessageRegistry registry;
+
+//initialize wire
+TwoWire customWire(PB9, PB8);
+
+//initialize dmp
+MPU9250DMP dmp = MPU9250DMP(customWire);
+
+// Initialize pressure sensor
+Adafruit_BMP280 bmp(&customWire);
+
+// Define pins
+#define HC12 Serial3
+
 void setup()
 {
-  // put your setup code here, to run once:
+  //Establish data connection first
+  HC12.begin(9600);
+  registry.addMessage(&basics);
+  registry.sendDataSchema(&HC12);
+
+  //now initialize the sensors
+  customWire.begin();
+  dmp.begin();
+  if (!bmp.begin(0x76))
+    registry.stream->addMessage("Error initializing bmp sensor");
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  //read all sensors and write output to transfer messages
+  dmp.readAccel();
+  dmp.readGyro();
+  dmp.updateReadables();
+  pitch.value = dmp.pitch;
+  bank.value = dmp.bank;
+  heading.value = dmp.yaw * 180 / PI;
+
+  altitude.value = bmp.readAltitude();
+
+  //write messages
+  basics.send(&HC12);
+  registry.stream->send(&HC12);
+  registry.readDataInStream(&HC12);
+  while (registry.stream->available())
+  {
+    registry.stream->addMessage("revieved: " + registry.stream->readMessage());
+  }
 }
